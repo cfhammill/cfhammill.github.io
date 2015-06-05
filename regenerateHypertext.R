a591bf58b0592086ac5d20541c943e4630df70f8
datamancy <cfhammill@gmail.com>
2015-06-04 23:13:04 -0230

    Added and fixed a lot

--- regenerateHypertext.R
+++ regenerateHypertext.R
@@ -2,14 +2,15 @@
 
 #Regenerate html
 
+# Index
+rmarkdown::render("index.rmd")
+
 # Posts
 posts <- paste0("_rawPosts/", list.files(path = "_rawPosts/", pattern = "*.[rR]md"))
 invisible(lapply(posts, rmarkdown::render, output_dir = "../posts"))
 
-# Index
-rmarkdown::render("index.rmd")
-
 # Pages
+opts_knit$set(child.dir = "../pages")
 pages <- paste0("_rawPages/", list.files(path = "_rawPages/", pattern = "*.[rR]md"))
 invisible(lapply(pages, rmarkdown::render, output_dir = "../pages"))
 
