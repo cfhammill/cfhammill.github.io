3640b1b5794306a31ecafa79971912bb1af718c2
datamancy <cfhammill@gmail.com>
2015-08-26 20:02:59 -0230

    Updated rmarkdown and knitr, fixed regenerateHypertext added new guest post

--- regenerateHypertext.R
+++ regenerateHypertext.R
@@ -7,10 +7,10 @@ rmarkdown::render("index.rmd")
 
 # Posts
 posts <- paste0("_rawPosts/", list.files(path = "_rawPosts/", pattern = "*.[rR]md"))
-invisible(lapply(posts, rmarkdown::render, output_dir = "../posts"))
+invisible(lapply(posts, rmarkdown::render, output_dir = "posts"))
 
 # Pages
 opts_knit$set(child.dir = "../pages")
 pages <- paste0("_rawPages/", list.files(path = "_rawPages/", pattern = "*.[rR]md"))
-invisible(lapply(pages, rmarkdown::render, output_dir = "../pages"))
+invisible(lapply(pages, rmarkdown::render, output_dir = "pages"))
 
